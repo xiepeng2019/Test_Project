0dfb95b59c9517abc4701ff83ebffe8d00917ada
xiepeng <1357736216@qq.com>
2020-08-15 22:17:10 +0800

    默认的

--- Test_Python/基础语法/写入文件.py
+++ Test_Python/基础语法/写入文件.py
@@ -11,5 +11,7 @@ def ord_user_lib_task_cancel_all_video_permissions_delete_task_restart_task_info
     random_str = get_random_name()
     task_name = "人像库" + random_str
     obj_data = TscSC(host,log,user,alarm=True,pre_user=True,pre_user_name=BaseOperation.pre_test_user1,create_video=True,custom_builds={
-        "rtsp":{}
-    })
+        "rtsp":{"count":1,"access":["face"],"path":["rtsp://10.9.244.23:8554/lz4-5.264"]}},pre_lib=True,pre_rtsp_typo=6
+    )
+    obj_data.switch_user(BaseOperation.pre_test_user1,BaseOperation.password)
+    photo_info = obj_data.pub_alrm.add_library_task(task_name=task_name,get_alarm=False,stop_stop=True)
